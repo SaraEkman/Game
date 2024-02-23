be85960ecee9d9b034f6bc4f4c0b1d88ef8e2f32
frankosgit <132131624+frankosgit@users.noreply.github.com>
2024-02-23 12:54:10 +0100

    tailwind install and daisy

--- game/src/App.tsx
+++ game/src/App.tsx
@@ -74,9 +74,20 @@ import './App.css'
   
   return (
     <>
+<div className="card w-96 bg-base-100 shadow-xl">
+  <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">CATS!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+
       <h1>Cat Quiz</h1>
       <div className="question-section">
-        <h2>
+        <h2 className='text-2xl'>
           <span>{currentQuestion + 1}</span>
           <br></br>Question:{questions[currentQuestion].question}
         </h2>
