975280cb9a6cfe25304b1b09d32d689bfc3b9c0b
sara <sara.ekmanMedieinstitutet@gmail.com>
2024-02-23 10:14:55 +0100

    add quiz array

--- game/src/App.tsx
+++ game/src/App.tsx
@@ -1,33 +1,49 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const quetions =
+    [
+    {
+      "question": "What is the average lifespan of a domestic cat?",
+      "options": ["5-10 years", "10-15 years", "15-20 years"],
+      "answer": "15-20 years"
+    },
+    {
+      "question": "Which cat is known for being hairless?",
+      "options": ["Maine Coon", "Sphynx", "Persian"],
+      "answer": "Sphynx"
+    },
+    {
+      "question": "What is a group of cats called?",
+      "options": ["A pack", "A clowder", "A pride"],
+      "answer": "A clowder"
+    },
+    {
+      "question": "Which cat breed is the largest?",
+      "options": ["Siamese", "Siberian", "Maine Coon"],
+      "answer": "Maine Coon"
+    },
+    {
+      "question": "How many whiskers does a cat typically have?",
+      "options": ["12", "24", "36"],
+      "answer": "24"
+    },
+  ]
+
+
+
   const [count, setCount] = useState(0)
+  const [isOn, setIsOn] = useState(false)
+
+  const handleClick = () => { 
+
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1></h1>
+    <button onClick={handleClick}></button>
     </>
   )
 }
